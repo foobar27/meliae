74cf64722b5c3ba57c14830739ae11f10ac68ccb
Sébastien Wagener <sebastien.wagener@gmail.com>
2018-12-23 13:19:30 +0100

    Add name prefix and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject meliae "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.github.foobar27/meliae "0.1.0-SNAPSHOT"
+  :description "Syntactic sugar for pattern matching"
+  :url "https://github.com/foobar27/meliae"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
