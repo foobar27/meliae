97958666398879750129b8a2de92e419994f60bd
Sébastien Wagener <sebastien.wagener@gmail.com>
2018-12-23 13:06:54 +0100

    Update clojure to 1.10

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [riddley "0.1.15"]
                  [org.clojure/core.match "0.3.0-alpha5"]])
